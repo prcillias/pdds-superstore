8fa82d1ed18d087e67b5adb66f6af65b4cffbfb1
fcnatasha <C14210071@john.petra.ac.id>
2023-12-15 01:39:56 +0700

    tes

--- helperMasalah3.php
+++ helperMasalah3.php
@@ -1,8 +1,10 @@
 <!-- cukup di run skali aja -->
 <?php 
 require 'connect.php';
-    $addRegion = "ALTER TABLE refunds ADD Region VARCHAR(255)";
-    $conn->query($addRegion);
-    $addState = "ALTER TABLE refunds ADD State VARCHAR(255)";
-    $conn->query($addState);
+    // $addRegion = "ALTER TABLE refunds ADD Region VARCHAR(255)";
+    // $conn->query($addRegion);
+    // $addState = "ALTER TABLE refunds ADD State VARCHAR(255)";
+    // $conn->query($addState);
+    $addCity = "ALTER TABLE refunds ADD City VARCHAR(255)";
+    $conn->query($addCity);
 ?>
