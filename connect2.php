7b1d4316036a4b28954c2462d63408cfb3643682
fcnatasha <C14210071@john.petra.ac.id>
2023-12-15 07:57:57 +0700

    Update connect2.php

--- connect2.php
+++ connect2.php
@@ -4,10 +4,10 @@
     $password = ""; // Replace with your database password
     $dbname = "superstore"; // Replace with your database name
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn2 = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
     if ($conn2->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn2->connect_error);
     }
  ?>
